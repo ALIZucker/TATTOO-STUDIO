7d0de94cb3d3c890433c15d92d01fc6c4c84a170
ALI_Zucker <zabihiali1998@gmail.com>
2024-02-12 00:35:54 +0330

    day2

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const lilist1 = document.createElement("li");
 const lilist2 = document.createElement("li");
 const lilist3 = document.createElement("li");
 const divnav = document.createElement("div");
-const articlenav = document.createElement("article");
+const divpost = document.createElement("div");
 ///////////////////////////////////////////setAttribute
 
 divbanner.setAttribute("id", "divbanner");
@@ -25,7 +25,7 @@ lilist3.setAttribute("class", "lilist");
 H1.setAttribute("id", "H1title");
 abot.setAttribute("id", "abot");
 divnav.setAttribute("id", "divnav");
-articlenav.setAttribute("class", "articlenav")
+divpost.setAttribute("id", "divpost");
 ///////////////////////////////////////////start Code
 /* بنر سایت */
 const img1 = createimg("assets/img/slider-default1.jpg")
@@ -48,10 +48,48 @@ divbanner.append(sectionleft, sectionright)
 demo.append(divbanner)
 
 /*لیست منو ها خواهد بود */
+const navtitle = ["TATTOOS &  <br/> COVER-UPS", "PIERCING &  <br/> JEWELLERY", "CUSTOM TATTOO  <br/> DESIGNS", "PORTRAITS  <br/> PAINTINGS"];
+const navpic = ["assets/img/logo.webp", "assets/img/about_image2.png", "assets/img/about_image3.png", "assets/img/about_image4.png"];
+
 for (let i = 0; i < 4; i++) {
 
+    divnav.append(createnav(navpic[i], navtitle[i]))
 }
-demo.append(divnav)
+
+demo.append(divnav);
+
+/*لیست پست هااا */
+const postimg = [
+    {
+        src: "assets/img/s1.jpg",
+        title: "GOTHIC TATTOO",
+        desc: "Sed eu justo sit amet metustin laoreet accum<br/> sanis asceyian ayli quam, umsausce"
+    }, {
+        src: "assets/img/s2.jpg",
+        title: "SIMPLE SHAPES",
+        desc: "Sed eu justo sit amet metustin laoreet accum <br/>sanis asceyian ayli quam, umsausce"
+    }, {
+        src: "assets/img/s3.jpg",
+        title: "FANTASY TATTOO",
+        desc: "Sed eu justo sit amet metustin laoreet accum<br/> sanis asceyian ayli quam, umsausce"
+    }, {
+        src: "assets/img/s4.jpg",
+        title: "JAPANESE TATTOO",
+        desc: "Sed eu justo sit amet metustin laoreet accum<br/> sanis asceyian ayli quam, umsausce"
+    }, {
+        src: "assets/img/s5.jpg",
+        title: "FULL SLEEVE TATTOO",
+        desc: "Sed eu justo sit amet metustin laoreet accum <br/>sanis asceyian ayli quam, umsausce"
+    }, {
+        src: "assets/img/s6.jpg",
+        title: "SKIN DEEP",
+        desc: "Sed eu justo sit amet metustin laoreet accum<br/> sanis asceyian ayli quam, umsausce"
+    }
+]
+for (let x of postimg) {
+    divpost.append(createpost(x.src,x.title,x.desc))
+}
+demo.append(divpost);
 
 ///////////////////////////////////////////////function
 
@@ -76,4 +114,34 @@ function carousel() {
     x[slideIndex - 1].style.display = "block";
     H1.innerHTML = title[slideIndex - 1]
     setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+function createnav(strimg, strtitle) {
+    const articlenav = document.createElement("article");
+    const imgnav = document.createElement("img");
+    const h2nav = document.createElement("h2");
+    articlenav.setAttribute("class", "articlenav")
+    imgnav.setAttribute("class", "imgnav")
+    h2nav.setAttribute("class", "h2nav")
+    imgnav.src = strimg;
+    h2nav.innerHTML = strtitle;
+    articlenav.append(imgnav, h2nav);
+    return articlenav;
+}
+
+function createpost(strimg, strtitle, strp) {
+    const articlepost = document.createElement("article");
+    const imgpost = document.createElement("img");
+    const h3post = document.createElement("h3");
+    const ppost = document.createElement("p");
+
+    articlepost.setAttribute("class", "articlepost")
+    imgpost.setAttribute("class", "imgpost")
+    h3post.setAttribute("class", "h3post")
+    ppost.setAttribute("class", "ppost")
+    imgpost.src = strimg;
+    h3post.innerHTML = strtitle;
+    ppost.innerHTML = strp;
+    articlepost.append(imgpost, h3post,ppost);
+    return articlepost;
 }
